328a93010134695bd2316e414f4a15a3c334e845
ale-strada <79310707+ale-strada@users.noreply.github.com>
2022-12-06 17:19:04 -0300

    page thanks

--- components/show-item/index.tsx
+++ components/show-item/index.tsx
@@ -1,4 +1,4 @@
-import { getOrder } from "lib/hooks";
+import { getOrder, useMe } from "lib/hooks";
 import router from "next/router";
 import styled from "styled-components";
 import { ButtonComprar } from "UI/buttons";
@@ -13,21 +13,30 @@ padding: 30px;
 `
 
 export function ShowItem({itemId, item}: any){
+    const user = useMe()
     function handleSubmit(e:any){
         e.preventDefault()
-        const address = e.target.address.value
+        if(user){
+        const address = e.target.address.value || user.address
         getOrder(itemId, address).then((res)=>{
-            console.log(res, "ORDER");
-            router.push(res.url)
+        router.push(res.url)
+        })
+        
+        }else{
+        const address = e.target.address.value 
+        getOrder(itemId, address).then((res)=>{
+        router.push(res.url)
         })
         }
+    }
+
 
     return <ShowItemConteiner>
         <img style={{width:"350px", height:"240px"}}src={item.Images[0].url}/>
         <Subtitle>{item.Name}</Subtitle>
         <Title> ${item["Unit cost"]}</Title>
         <FormResponsiveConteiner  onSubmit={handleSubmit}>
-            <TextField name="address" label="Envio" width="100%" placeholder="Direccion para enviar el producto"/>
+            <TextField name="address" label="Envio" width="100%" placeholder={"Direccion para enviar el producto" || user.addres} />
         <ButtonComprar>
             <Subtitle>ir a pagar</Subtitle>
         </ButtonComprar>   
---
+++ pages/thanks.tsx
@@ -0,0 +1,28 @@
+import { Footer } from "components/footer";
+import { Header } from "components/header";
+import Router from "next/router";
+import { PrimaryButton } from "UI/buttons";
+import { PageConteiner } from "UI/Layout";
+import { BodyBold, Subtitle, Title } from "UI/text";
+
+export default function Thanks() {
+    function handleClick(){
+        Router.push("/")
+    }
+    return <PageConteiner>
+        <Header/>
+        <div style={{
+            margin:"100px",
+            display:"flex",
+            flexDirection:"column",
+            alignItems:"center"
+            }}>
+            <Title> Gracias por su compra! </Title>
+            <Subtitle>su producto será enviado lo antes posible</Subtitle>
+            <PrimaryButton onClick={handleClick}>
+                <BodyBold> Ir a Home</BodyBold>
+            </PrimaryButton>
+        </div>
+        <Footer/>
+    </PageConteiner>
+}
