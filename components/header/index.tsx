cb339bed021891a80b0f256cc1af146f233399a8
ale-strada <79310707+ale-strada@users.noreply.github.com>
2022-12-06 12:52:29 -0300

    session manager burguer menu

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -5,7 +5,7 @@ import { MenuConteiner, MenuLink, MenuLinkConteiner } from "UI/menu-burguer"
 import { useState } from "react"
 import { ButtonIngresar } from "UI/buttons"
 import { Body, BodyBoldWhite, LargeText } from "UI/text"
-import router, { Router } from "next/router"
+import router from "next/router"
 import Link from "next/link"
 import { useMe } from "lib/hooks"
 import { UserDataConteiner } from "UI/Layout"
@@ -28,16 +28,12 @@ const BurguerConteiner = styled.div`
 function clickLogout(){
 router.push("logout")
 }
-function SessionManager(){
-const user = useMe()
-
+function SessionManager({user}:any){
     if(user){
-        console.log(user, "USERR");
         return <UserDataConteiner>
                     <LargeText style={{color:"var(--white)"}}>{user.email}</LargeText>
                     <Body onClick={clickLogout} style={{color:"var(--fucsia)", cursor:"pointer"}}>Cerrar sesión</Body>
                 </UserDataConteiner>
-        
     }else{
         return <ButtonIngresar onClick={()=>{router.push("/login")}}>
                     <BodyBoldWhite>Ingresar</BodyBoldWhite>
@@ -46,11 +42,12 @@ const user = useMe()
 }
 export function Header(){
     const [open, setOpen] = useState("none")
+    const user = useMe()
     return<HeaderConteiner>
         <div onClick={()=>{router.push("/")}}>
         <LogoWhite />
         </div>
-        <SessionManager/>
+        <SessionManager user={user}/>
         <BurguerConteiner>
             <Hamburger color="var(--white)" onToggle={toggled=>{toggled? setOpen(""):  setOpen("none")}}/>
         </BurguerConteiner>
@@ -64,6 +61,14 @@ export function Header(){
                     <Link style={{textDecoration:"none", color: "inherit"}} href="/search" passHref={true}>Buscar</Link> 
                 </MenuLink>
             </MenuLinkConteiner>
+            {user? <div style={{
+                display:"flex",
+                flexDirection:"column",
+                alignItems:"center", 
+                padding:"20px"}}>
+                    <LargeText style={{color:"var(--white)"}}>{user.email}</LargeText>
+                    <Body onClick={clickLogout} style={{color:"var(--fucsia)", cursor:"pointer"}}>Cerrar sesión</Body>
+                </div>: <div></div>}
         </MenuConteiner>
     </HeaderConteiner>
 }
